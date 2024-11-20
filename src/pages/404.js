d27f2a7a365b9f9dc61b2c7b655cf40bde7720c2
Aleksandra Fabian <pleolka@gmail.com>
2024-11-20 23:58:38 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Layout from "../layout/layout"
+import { ContainerMob, Heading } from "../utils/utils"
 
 const Page404 = () => {
   return (
     <Layout>
-      <h1>404</h1>
-      <h6>
-        <span>Niestety szukana strona nie istnieje</span>
-      </h6>
+      <ContainerMob>
+        <Heading>
+          <h1>404</h1>
+          <h6>
+            <span>Niestety szukana strona nie istnieje</span>
+          </h6>
+        </Heading>
+      </ContainerMob>
     </Layout>
   )
 }
