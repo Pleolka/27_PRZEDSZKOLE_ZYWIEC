b37423e5b24fe49615d6c029940613e905593498
Aleksandra Fabian <pleolka@gmail.com>
2024-11-20 21:12:57 +0100

    poprawki ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,40 +1,41 @@
 import React from "react"
-import { wrapRootElement as wrap } from "./root-wrapper"
 import { ServerStyleSheet } from "styled-components"
+import { wrapRootElement as wrap } from "./root-wrapper"
 
 export const wrapRootElement = wrap
 
 export const onRenderBody = ({ setHeadComponents }) => {
   const sheet = new ServerStyleSheet()
-  const styleTags = sheet.getStyleElement() // Pobiera style wygenerowane przez Styled Components
 
-  setHeadComponents([
-    // Preconnect do serwerów Google Fonts
-    <link
-      key="google-fonts-preconnect"
-      rel="preconnect"
-      href="https://fonts.googleapis.com"
-    />,
-    <link
-      key="google-fonts-preconnect-crossorigin"
-      rel="preconnect"
-      href="https://fonts.gstatic.com"
-      crossOrigin="anonymous"
-    />,
-    // Preload krytycznych czcionek
-    <link
-      key="google-fonts-preload"
-      rel="preload"
-      href="https://fonts.googleapis.com/css2?family=Afacad+Flux&family=Pacifico&family=Yaldevi&display=swap"
-      as="style"
-    />,
-    // Styl czcionek Google Fonts
-    <link
-      key="google-fonts-stylesheet"
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Afacad+Flux&family=Pacifico&family=Yaldevi&display=swap"
-    />,
-    // Wstrzyknięcie stylów Styled Components do <head>
-    ...styleTags,
-  ])
+  try {
+    // Pobierz wygenerowane style Styled Components
+    const styleTags = sheet.getStyleElement()
+
+    // Dodaj style Styled Components do <head>
+    setHeadComponents([
+      ...styleTags,
+      // Preconnect do Google Fonts
+      <link
+        key="google-fonts-preconnect"
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+      />,
+      <link
+        key="google-fonts-preconnect-crossorigin"
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />,
+      // Link do Google Fonts z `font-display: swap`
+      <link
+        key="google-fonts-stylesheet"
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Afacad+Flux&family=Pacifico&family=Yaldevi&display=swap"
+      />,
+    ])
+  } catch (error) {
+    console.error("Error during SSR with styled-components:", error)
+  } finally {
+    sheet.seal()
+  }
 }
